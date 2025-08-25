398ebd4c9073c0adf1350437dafa0e67c4b67907
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-25 23:06:48 +0300

    Add Room model

--- src/apps/rooms/models.py
+++ src/apps/rooms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+from common.mixins import TimeStampMixin
 
-# Create your models here.
+
+class Room(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    active = models.BooleanField(default=True)
+
+    def __repr__(self):
+        return f"<{self.id}> Room {self.name} - {self.price}"
+
+    def __str__(self):
+        return self.name
