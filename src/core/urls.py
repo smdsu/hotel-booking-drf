096435b0f8827b8a1ecef18ca98fc51e955e7fb4
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-26 21:35:26 +0300

    Refactor URL routing in core to remove namespace from rooms and booking includes

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,6 +21,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/v1/", include(("rooms.urls", "rooms"), namespace="v1")),
-    path("api/v1/", include(("booking.urls", "booking"), namespace="v1")),
+    path("api/v1/", include(("rooms.urls", "rooms"))),
+    path("api/v1/", include(("booking.urls", "booking"))),
 ]
