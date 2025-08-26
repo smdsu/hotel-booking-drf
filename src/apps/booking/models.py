cf4ec5b1cb90639ac5e84eff43121d7d1b6e9c14
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-26 20:18:03 +0300

    Add Booking model

--- src/apps/booking/models.py
+++ src/apps/booking/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from common.mixins import TimeStampMixin
+
+class Booking(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE, related_name="bookings")
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
