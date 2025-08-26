ea275a6bc252b787e305e125d00311881952d6f7
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-26 20:12:47 +0300

    Update URL routing to include DRF Spectacular schema and Swagger UI endpoints

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/v1/", include(("rooms.urls", "rooms"), namespace="v1")),
 ]
