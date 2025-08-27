11249ebbca6e71a3063854de6bdb96fdd1a00165
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-27 18:09:36 +0300

    Update RoomViewSet to dynamically select serializer based on action, using RoomDetailResponseSerializer for retrieve action

--- src/apps/rooms/views.py
+++ src/apps/rooms/views.py
@@ -2,11 +2,16 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 from .models import Room
-from .serializers import RoomSerializer, RoomCreateResponseSerializer
+from .serializers import RoomSerializer, RoomCreateResponseSerializer, RoomDetailResponseSerializer
+
 
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return RoomDetailResponseSerializer
+        return RoomSerializer
 
     @extend_schema(
             request=RoomSerializer,
