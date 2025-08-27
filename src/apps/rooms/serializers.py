fba08d02c2ae56504774ef163992f1bc712ad121
smdsu <109036946+smdsu@users.noreply.github.com>
2025-08-27 18:09:15 +0300

    Add RoomDetailResponseSerializer to include bookings in room details

--- src/apps/rooms/serializers.py
+++ src/apps/rooms/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
+from booking.serializers import BookingSerializer
 from .models import Room
+
 
 class RoomSerializer(ModelSerializer):
     class Meta:
@@ -10,3 +12,10 @@ class RoomCreateResponseSerializer(ModelSerializer):
     class Meta:
         model = Room
         fields = ["room_id"]
+
+class RoomDetailResponseSerializer(ModelSerializer):
+    bookings = BookingSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = ["room_id", "price", "bookings"]
